2d9afff1e0901733bdc4314fa222115f984dc374
cdewantoro <dewantoro@live.com>
2023-05-31 15:06:19 +0700

    add connection.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+
+
+$host = "db4free.net:3306";
+$user = "genbox";
+$pass = "genbox12345";
+$DB = "genboxpulsaweb";
+
+$koneksi = mysqli_connect($host,$user,$pass,$DB);
+
+if(!$koneksi){
+    echo "Error: ".mysqli_error($koneksi);
+} 
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
